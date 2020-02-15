b74f3be4398e59d8cd6477adb7e162de7c26368f
vseventer <mark@vseventer.com>
2020-02-14 21:45:21 -0500

    Save CNAME to dist.

--- src/CNAME.11ty.js
+++ src/CNAME.11ty.js
@@ -1,19 +1,27 @@
 // @see https://www.11ty.io/docs/languages/javascript/
 
 // Standard lib.
+import { join as joinPath } from 'path';
 import { URL as NodeURL } from 'url';
 
 // Package modules.
-import { homepage } from '../package.json';
+import {
+  config,
+  homepage
+} from '../package.json';
 
 // Constants.
-const CNAME_FILE = 'CNAME';
+const OUTPUT_DIRECTORY = config.output;
+const CNAME_FILE = joinPath(OUTPUT_DIRECTORY, 'CNAME');
 
 // Exports.
 module.exports = class CNameRecord {
   #hostname = null;
 
-  data = { permalink: CNAME_FILE }
+  data = {
+    permalink: CNAME_FILE,
+    permalinkBypassOutputDir: true
+  }
 
   constructor() {
     // Extract hostname from homepage.
